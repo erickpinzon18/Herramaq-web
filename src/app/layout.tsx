34cad2b6106746ef50f5816fba3093cb8219fec9
Erick Pinzon <erickpinzonhuerta@gmail.com>
2026-03-10 14:01:16 -0600

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ChatWidget } from "./components/ChatWidget";
 import { AuthProvider } from "@/contexts/AuthContext";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -297,6 +298,7 @@ export default function RootLayout({
           {children}
           {/* <ChatWidget /> */}
         </AuthProvider>
+        <Analytics />
       </body>
     </html>
   );
